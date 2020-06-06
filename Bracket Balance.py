bcac3611489dac46904f02e4fd43041d027368da
Ankit Jain <akjain6067.aj@gmail.com>
2020-06-06 16:39:49 +0530

    Add files via upload
    Solved bracket balance problem on hackerearth using stack in python

---
+++ Bracket Balance.py
@@ -0,0 +1,20 @@
+n = int(input())
+for _ in range(n):
+    breck_seq = input()
+    li = []
+    for breck in breck_seq:
+        try:
+            if li[-1] == '[' and breck == ']':
+                del li[-1]
+            elif li[-1] == '{' and breck == '}':
+                del li[-1]
+            elif li[-1] == '(' and breck == ')':
+                del li[-1]
+            elif breck == '[' or '{' or '(':
+                li.append(breck)
+        except IndexError:
+            li.append(breck)
+    if len(li) == 0:
+        print('YES')
+    else:
+        print('NO')
