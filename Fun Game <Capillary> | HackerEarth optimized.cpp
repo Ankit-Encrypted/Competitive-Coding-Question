c254fd0924623c81374e82b68e2c2db73673d2e4
Ankit Jain <akjain6067.aj@gmail.com>
2020-06-07 21:58:24 +0530

    Fun Game <Capillary>| optimized solution | HackerEarth | C++

---
+++ Fun Game <Capillary> | HackerEarth optimized.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int i = 0, j = n - 1;
+    while (i < n && j >= 0) {
+        if (a[i] > a[j]) {
+            cout << "1" << " ";
+            j--;
+        } else if (a[i] < a[j]) {
+            cout << "2" << " ";
+            i++;
+        }  else
+        {
+            cout << "0" << " ";
+            i++, j--;
+        }
+        
+    }
+    return 0;
+}
